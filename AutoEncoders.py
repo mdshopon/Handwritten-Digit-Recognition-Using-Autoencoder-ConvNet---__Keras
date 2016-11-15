14c11805a47a0bc15a53af70d0b93a55cc5cd038
Md Shopon <shopon.uap@gmail.com>
2016-11-15 18:14:21 +0600

    Update AutoEncoders.py

--- AutoEncoders.py
+++ AutoEncoders.py
@@ -115,15 +115,14 @@ def YCMATERTEST():
         Test.append(i % 10)
     return Test
 
+# Dataset Preparation
 Name2=XCMATERTEST()
 Name=XINDIANTRAIN()
 
-# print(Name)
 
 y_test=YCMATERTEST()
 y_train=YINDIANTRAIN()
 
-# print(y_train)
 
 X_train = np.array([np.array(Image.open(fname)) for fname in Name])
 X_test = np.array([np.array(Image.open(fname)) for fname in Name2])
@@ -138,129 +137,97 @@ Y_train = np_utils.to_categorical(y_train, nb_classes)
 Y_test = np_utils.to_categorical(y_test, nb_classes)
 
 
-# print(X_train.shape)
-# print(X_test.shape)
-
-MODEL=load_model('shopon-model.h5')
-score=MODEL.evaluate(X_test,Y_test,verbose=0)
-
-print('Test score:', score[0])
-print('Test accuracy:', score[1])
-#j=3000
-# for i in range(0, 9):
-#     pyplot.subplot(330 + 1 + i)
-#     pyplot.imshow(X_test[j], cmap=pyplot.get_cmap('gray'))
-#     j=j+1
-# pyplot.show()
 
 
 
 
 #Autoencoder Part:
-#
-# input_img = Input(shape=(1, 32, 32))
-#
-# x = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(input_img)
-# x = MaxPooling2D((2, 2), border_mode='same')(x)
-# x = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(x)
-# x = MaxPooling2D((2, 2), border_mode='same')(x)
-# x = Dropout(0.35)(x)
-# x = Convolution2D(16, 3, 3, activation='relu', border_mode='same')(x)
-# encoded = MaxPooling2D((2, 2), border_mode='same')(x)
-#
-# # at this point the representation is (32, 7, 7)
-#
-# x = Convolution2D(16, 3, 3, activation='relu', border_mode='same')(encoded)
-# x = UpSampling2D((2, 2))(x)
-# x = Dropout(0.35)(x)
-# x = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(x)
-# x = UpSampling2D((2, 2))(x)
-# x = Convolution2D(128 , 3, 3, activation='relu', border_mode='same')(x)
-# x = UpSampling2D((2, 2))(x)
-# decoded = Convolution2D(1, 3, 3, activation='sigmoid', border_mode='same')(x)
-#
-# autoencoder = Model(input_img, decoded)
-# autoencoder.compile(optimizer='RMSprop', loss='binary_crossentropy')
-#
-# datagen = ImageDataGenerator(
-#
-#     rotation_range=0.8,
-#     width_shift_range=0.3,
-#     height_shift_range=0.2
-# )
-#
-# datagen.fit(X_train,augment=True)
-#
-# autoencoder.fit_generator(datagen.flow(X_train, X_train, batch_size=128,shuffle=True),
-#                           nb_epoch=60,
-#                           samples_per_epoch=19313,
-#                           validation_data=(X_test, X_test)
-#                           )
-#
-# # autoencoder.fit_generator(X_train, X_train,
-# #                 nb_epoch=100,
-# #                 batch_size=128,
-# #                 shuffle=True,
-# #                 validation_data=(X_test, X_test),
-# #                 callbacks = [TensorBoard(log_dir='/tmp/oencoder3')]
-# #                 )
-#
-# #autoencoder.load_weights('autoencoder.h5')
-# #encoded_imgs = autoencoder.predict(X_train)
-# decoded_imgs = autoencoder.predict(X_test)
-#
-# #autoencoder.save('autoencoder.h5')
-#
-# #Supervised Learning Part
-# x=Convolution2D(256, 4, 4, activation='relu', border_mode='same')(encoded)
-# x=MaxPooling2D((2, 2), border_mode='same')(x)
-# x=Dropout(0.35)(x)
-# x=Convolution2D(128, 2, 2, activation='relu', border_mode='same')(x)
-# x=MaxPooling2D((2, 2), border_mode='same')(x)
-# x=Flatten()(x)
-# x=Dense(512,activation='relu')(x)
-# x=Dense(128,activation='relu')(x)
-# x=Dropout(0.35)(x)
-# x=Dense(50,activation='relu')(x)
-# x=Dense(10,activation='softmax')(x)
-# MODEL=Model([input_img],[x])
-#
-#
-#
-# MODEL.compile(loss='categorical_crossentropy',
-#               optimizer='RMSprop',
-#               metrics=['accuracy'])
-#
-#
-# train_datagen = ImageDataGenerator(
-#          rotation_range=0.3,
-#          width_shift_range=0.2,
-#         height_shift_range=0.2
-#         )
-# train_datagen.fit(X_train, augment=True)
-# test_datagen = ImageDataGenerator(
-#
-# )
-#
-# train_generator = train_datagen.flow(
-#          X_train, Y_train,
-#          batch_size=batch_size)
-# validation_generator = test_datagen.flow(
-#         X_test,Y_test,
-#         batch_size=batch_size)
-#
-# from keras.callbacks import TensorBoard
-#
-# MODEL.fit_generator(
-#         train_generator,
-#         samples_per_epoch=19313,
-#         nb_epoch=120,
-#         validation_data=validation_generator,
-#         nb_val_samples=3986,
-#         callbacks = [TensorBoard(log_dir='/tmp/oencoder3')])
-#
-#
-#
-# score = MODEL.evaluate(X_test, Y_test, verbose=0)
-# print('Test score:', score[0])
-# print('Test accuracy:', score[1])
+
+input_img = Input(shape=(1, 32, 32))
+
+#Encoder
+x = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(input_img)
+x = MaxPooling2D((2, 2), border_mode='same')(x)
+x = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(x)
+x = MaxPooling2D((2, 2), border_mode='same')(x)
+x = Dropout(0.35)(x)
+x = Convolution2D(16, 3, 3, activation='relu', border_mode='same')(x)
+encoded = MaxPooling2D((2, 2), border_mode='same')(x)
+
+#Decoder
+x = Convolution2D(16, 3, 3, activation='relu', border_mode='same')(encoded)
+x = UpSampling2D((2, 2))(x)
+x = Dropout(0.35)(x)
+x = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(x)
+x = UpSampling2D((2, 2))(x)
+x = Convolution2D(128 , 3, 3, activation='relu', border_mode='same')(x)
+x = UpSampling2D((2, 2))(x)
+decoded = Convolution2D(1, 3, 3, activation='sigmoid', border_mode='same')(x)
+
+autoencoder = Model(input_img, decoded)
+autoencoder.compile(optimizer='RMSprop', loss='binary_crossentropy')
+
+#Autoencoder Augmention
+datagen = ImageDataGenerator(
+
+    rotation_range=0.8,
+    width_shift_range=0.3,
+    height_shift_range=0.2
+)
+
+datagen.fit(X_train,augment=True)
+
+autoencoder.fit_generator(datagen.flow(X_train, X_train, batch_size=128,shuffle=True),
+                          nb_epoch=60,
+                          samples_per_epoch=19313,
+                          validation_data=(X_test, X_test)
+                          )
+
+
+decoded_imgs = autoencoder.predict(X_test)
+
+
+#Supervised Learning Part
+x=Flatten()(encoded)
+x=Dense(128,activation='relu')(x)
+x=Dense(10,activation='softmax')(x)
+MODEL=Model([input_img],[x])
+
+
+MODEL.compile(loss='categorical_crossentropy',
+              optimizer='RMSprop',
+              metrics=['accuracy'])
+
+
+train_datagen = ImageDataGenerator(
+         rotation_range=0.3,
+         width_shift_range=0.2,
+        height_shift_range=0.2
+        )
+train_datagen.fit(X_train, augment=True)
+test_datagen = ImageDataGenerator(
+
+)
+
+train_generator = train_datagen.flow(
+         X_train, Y_train,
+         batch_size=batch_size)
+validation_generator = test_datagen.flow(
+        X_test,Y_test,
+        batch_size=batch_size)
+
+from keras.callbacks import TensorBoard
+
+MODEL.fit_generator(
+        train_generator,
+        samples_per_epoch=19313,
+        nb_epoch=120,
+        validation_data=validation_generator,
+        nb_val_samples=3986,
+        callbacks = [TensorBoard(log_dir='/tmp/oencoder3')])
+
+
+
+score = MODEL.evaluate(X_test, Y_test, verbose=0)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
